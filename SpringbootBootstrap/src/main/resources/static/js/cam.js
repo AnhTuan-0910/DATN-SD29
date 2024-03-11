9078dce39670f6f6705bb5331b604364ad16e79a
AnhTuan-0910 <tn081197@gmail.com>
2024-03-11 19:55:31 +0700

    tuan_sua_cam

--- SpringbootBootstrap/src/main/resources/static/js/cam.js
+++ SpringbootBootstrap/src/main/resources/static/js/cam.js
@@ -27,5 +27,31 @@ domReady(function () {
         "my-qr-reader",
         { fps: 10, qrbos: 250 }
     );
+    let htmlscanner1 = new Html5QrcodeScanner(
+        "my-qr-reader-2",
+        { fps: 10, qrbos: 250 }
+    );
+    let htmlscanner2 = new Html5QrcodeScanner(
+        "my-qr-reader-3",
+        { fps: 10, qrbos: 250 }
+    );
+    let htmlscanner3 = new Html5QrcodeScanner(
+        "my-qr-reader-4",
+        { fps: 10, qrbos: 250 }
+    );
+    let htmlscanner4 = new Html5QrcodeScanner(
+        "my-qr-reader-5",
+        { fps: 10, qrbos: 250 }
+    );
+    let htmlscanner5 = new Html5QrcodeScanner(
+        "my-qr-reader-6",
+        { fps: 10, qrbos: 250 }
+    );
     htmlscanner.render(onScanSuccess);
+    htmlscanner1.render(onScanSuccess);
+    htmlscanner2.render(onScanSuccess);
+    htmlscanner3.render(onScanSuccess);
+    htmlscanner4.render(onScanSuccess);
+    htmlscanner5.render(onScanSuccess);
+
 });
