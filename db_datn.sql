1c91cb3f4fe21b81c2208485b91128961b39e99b
AnhTuan-0910 <128662442+AnhTuan-0910@users.noreply.github.com>
2024-03-13 22:07:26 +0700

    Update db_datn.sql

--- db_datn.sql
+++ db_datn.sql
@@ -294,3 +294,17 @@ create table khuyen_mai(
 	sua_boi nvarchar(20),
 	
 )
+
+CREATE TRIGGER trg_generate_ma_hoa_don
+ON hoa_don
+FOR INSERT 
+AS 
+BEGIN
+DECLARE @id_hoa_don uniqueidentifier;
+DECLARE @ma_hoa_don VARCHAR(20);
+
+SELECT @id_hoa_don = id_hoa_don FROM inserted;
+SET @ma_hoa_don = 'HD' + RIGHT(CONVERT(VARCHAR(36), @id_hoa_don), 6);
+
+UPDATE hoa_don SET ma = @ma_hoa_don WHERE id_hoa_don = @id_hoa_don;
+END;
